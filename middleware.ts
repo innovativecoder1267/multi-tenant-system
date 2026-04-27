bf7c5ad35a19c2fac2b8ab6e2cc5696fdf72342f
innovativecoder1267 <aaravchaprana1627@gmail.com>
2026-04-27 13:57:30 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -26,7 +26,7 @@ export default async function middleware(req: NextRequest) {
    if (token && pathname === "/login") {
     return NextResponse.redirect(new URL("/dashboard", req.url));
   }
-  if(token && pathname === "/dashboard"){
+  if(token){
     return NextResponse.redirect(new URL("/dashboard/analytics",req.url))
   }
   return NextResponse.next();
