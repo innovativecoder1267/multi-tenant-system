748119139c80159af00e4a1ae3e975f19f02e1f6
innovativecoder1267 <aaravchaprana1627@gmail.com>
2026-05-05 13:46:55 +0530

    Update page.tsx

--- src/app/verifyotp/page.tsx
+++ src/app/verifyotp/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Suspense } from "react";
-import VerifyContent from "./VerifyContent";
+import VerifyContent from "./verifyotp";
 
 export default function VerifyPage() {
   return (
