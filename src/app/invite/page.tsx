b26c3b55673abc0edb7b8df6ac304f0c24feeaa2
innovativecoder1267 <aaravchaprana1627@gmail.com>
2026-06-13 15:50:28 +0530

    Update page.tsx

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,7 +1,13 @@
+import { Suspense } from "react";
 import InvitePage from "./invite";
+ 
 
-export const dynamic = "force-dynamic";
-
-export default function Page() {
-  return <InvitePage />;
+export default function Page(){
+  return (
+  <Suspense fallback ={<div>...Loading</div>}>
+  <div >
+    <InvitePage/>
+  </div>
+  </Suspense>  
+  )
 }
